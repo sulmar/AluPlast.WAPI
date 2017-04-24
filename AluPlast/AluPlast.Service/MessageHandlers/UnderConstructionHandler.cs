523d4d85031a60eb827627d4cba1f13fb59cfe64
Marcin Sulecki <marcin.sulecki@gmail.com>
2017-04-24 16:17:58 +0200

    Przykład handlera blokującego przepływ

---
+++ AluPlast/AluPlast.Service/MessageHandlers/UnderConstructionHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AluPlast.Service.MessageHandlers
+{
+    public class UnderConstructionHandler : DelegatingHandler
+    {
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var response = new HttpResponseMessage(System.Net.HttpStatusCode.ServiceUnavailable);
+
+            return Task.FromResult(response);
+        }
+    }
+}
