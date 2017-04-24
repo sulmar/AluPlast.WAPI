f23e1433e111ea9093098a4c444a0be4ff5fa009
Marcin Sulecki <marcin.sulecki@gmail.com>
2017-04-24 16:17:33 +0200

    Dodanie weryfikacji klucza za pomocą handlera

---
+++ AluPlast/AluPlast.Service/MessageHandlers/SecretKeyHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AluPlast.Service.MessageHandlers
+{
+    public class SecretKeyHandler : DelegatingHandler
+    {
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            IEnumerable<string> secretKeys;
+
+            if (request.Headers.TryGetValues("Secret-Key", out secretKeys))
+            {
+                if (secretKeys.Any(s=>s == "12345"))
+                {
+                    return await base.SendAsync(request, cancellationToken);
+                }
+            }
+
+            var response = new HttpResponseMessage(System.Net.HttpStatusCode.Forbidden);
+
+            return response;
+        }
+    }
+}
