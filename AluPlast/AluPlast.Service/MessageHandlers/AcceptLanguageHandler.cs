1ae87067525d9f766788ae6a5b8dd4cb5652ae8e
Marcin Sulecki <marcin.sulecki@gmail.com>
2017-04-27 15:24:21 +0200

    Ustawienie języka

---
+++ AluPlast/AluPlast.Service/MessageHandlers/AcceptLanguageHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AluPlast.Service.MessageHandlers
+{
+    public class AcceptLanguageHandler : DelegatingHandler
+    {
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var language = request.Headers.AcceptLanguage.FirstOrDefault();
+
+            if (language!=null)
+            {
+                Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo(language.Value);
+            }
+
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
