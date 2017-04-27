d5e373861628b40fa503999db1e675421287404f
Marcin Sulecki <marcin.sulecki@gmail.com>
2017-04-27 11:24:55 +0200

    Wyświetlenie strony html

---
+++ AluPlast/AluPlast.Service/ActionResults/HtmlActionResult.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace AluPlast.Service.ActionResults
+{
+    public class HtmlActionResult : IHttpActionResult
+    {
+        private readonly string html;
+
+        public HtmlActionResult(string html)
+        {
+            this.html = html;
+        }
+
+        public Task<HttpResponseMessage> ExecuteAsync(CancellationToken cancellationToken)
+        {
+            var response = new HttpResponseMessage(System.Net.HttpStatusCode.OK);
+
+            response.Content = new StringContent(html);
+
+            response.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("text/html");
+
+            return Task.FromResult(response);
+
+        }
+    }
+}
---
+++ AluPlast/AluPlast.Service/Controllers/DashboardController.cs
@@ -0,0 +1,31 @@
+﻿using AluPlast.ControlLoader.Interfaces;
+using AluPlast.Models.SearchCriterias;
+using AluPlast.Service.ActionResults;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace AluPlast.Service.Controllers
+{
+
+    [Route("dashboard")]
+    public class DashboardController : ApiController
+    {
+        private readonly ILoadsService LoadService;
+
+        public DashboardController(ILoadsService loadService)
+        {
+            this.LoadService = loadService;
+        }
+
+        public IHttpActionResult Get()
+        {
+            var load = LoadService.Get(1);
+
+            return new HtmlActionResult("<b>Hello</b>");
+        }
+    }
+}
